894d3ce20197d89961a5dcba9a6eabd9a30f08dc
mickmed <mick2090@gmail.com>
2020-02-11 11:59:38 -0500

    init; :

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,15 +1,6 @@
-let algos = require("../groupArrays.js")
+let algos = require("../repeatStringNumTimes.js")
 
 test("groups the arrays into an array of arrays of given length, any remainding go in last array", () => {
-  expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([
-    ["a", "b", "c", "d"]
-  ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([
-    ["a", "b", "c"],
-    ["d"]
-  ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([
-    ["a", "b"],
-    ["c", "d"]
-  ])
+  expect(algos.repeatStringNumTimes("abc", 3)).toBe("abcabcabc")
+
 })
