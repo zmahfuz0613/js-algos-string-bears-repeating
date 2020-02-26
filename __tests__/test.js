81666dada2a4c50acdf62dca0d6f9c6552279c3a
mickmed <mick2090@gmail.com>
2020-02-26 16:15:42 -0500

    mods

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,9 @@
 let algos = require("../repeatStringNumTimes.js")
 
-test("groups the arrays into an array of arrays of given length, any remainding go in last array", () => {
+test("repeat the string N times", () => {
   expect(algos.repeatStringNumTimes("abc", 3)).toBe("abcabcabc")
+  expect(algos.repeatStringNumTimes("fine time", 3)).toBe("fine timefine timefine time")
+  expect(algos.repeatStringNumTimes("hi there friend", 2)).toBe("hi there friendhi there friend")
+
 
 })
